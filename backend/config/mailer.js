66f827637e46942ea28f8a32f8ad1f9aab3debf2
Fahmid Chowdhury <fahmidoct26@gmail.com>
2024-07-06 01:49:28 +0600

    Update mailer.js

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -17,7 +17,7 @@ export const sendEmail = async (to, subject, text) => {
 
 try {
     const info = await transporter.sendMail({
-    from: `"Fahmid" <${process.env.EMAIL}>`,
+    from: `"WheelZOnRent" <${process.env.EMAIL}>`,
     to,
     subject,
     text
